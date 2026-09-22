9b97f90e31039bddac078e239fbba1fe65031258
Luis Marinho <luisdm5309@gmail.com>
2026-09-22 16:39:56 -0300

    Cria componente de Navbar

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css'
+import Navbar from "./components/Navbar"
 
 function App() {
-  return <><h1>Hello, World!</h1></>
+  return <>
+    <Navbar />
+  </>
 }
 
-export default App
+export default App;
--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+
 import App from './App.tsx';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
