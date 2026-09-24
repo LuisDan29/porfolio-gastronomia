711fb83fa156885a7d1f004100297b14d8427628
Luis Marinho <luisdm5309@gmail.com>
2026-09-24 00:22:13 -0300

    Adiciona imagens na Hero section

--- portfolio/src/components/Hero/Hero.tsx
+++ portfolio/src/components/Hero/Hero.tsx
@@ -8,15 +8,28 @@ function Hero() {
           <h1 className="hero-title display-1 text-primary">PORTFÓLIO<br/>GASTRO</h1>
         </div>  
         <div className="col-2 p-0 d-flex flex-column justify-content-end">
-          <p className="text m-0 fs-5 text-primary">A GASTRONOMIA É A<br/>NOSSA ARTE.</p>
+          <p className="text1 m-0 fs-5 text-primary">A GASTRONOMIA É A<br/>NOSSA ARTE.</p>
         </div>
       </div>
       <div className="row p-0">
         <div className="col-1 p-0 offset-7">
-          <span class="material-symbols-outlined arrow-icon text-primary">arrow_outward</span>
+          <span class="material-symbols-outlined arrow-icon1 text-primary">arrow_outward</span>
         </div>
       </div>
-      <div className="row bg-success"></div>
+      <div className="row">
+        <div className="col-8 p-0 offset-2 d-flex flex-row align-items-end">
+          <p className="text2 m-0 fs-2 text-primary">CONHEÇA NOSSO<br/>TRABALHO.</p>
+          <span class="material-symbols-outlined ms-5 arrow-icon2 text-primary">arrow_outward</span>
+        </div>
+        <div className="col-2">
+          <div className="hero-image1 p-0" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4 offset-5 pt-5 px-5">
+          <div className="hero-image2 p-0"></div>
+        </div>
+      </div>
     </section>
   )
 }
